7d1fd1286bbe455296b708da4a29053e54ad4f0b
nshebeko <nikolay.shebeko@gmail.com>
2020-02-28 20:49:02 +0300

    init: init branch Styleguide

---
+++ culture-portal/src/components/Styleguide/Styleguide.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Styleguide = () =>{
+  console.log('init Styleguide')
+
+return <div>init</div>
+}
+
+export default Styleguide;
